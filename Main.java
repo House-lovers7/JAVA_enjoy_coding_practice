b4af71376fd0ef225120cc8f5aa85a840aecfe8f
house-lovers7 <taisuke.gt@gmail.com>
2021-06-19 11:02:21 +0900

    ”変数設定と型

--- Main.java
+++ Main.java
@@ -9,5 +9,24 @@ class Main {
     msg += "Hello ";
     msg += "JAVA!";
     System.out.println(msg);
+
+    // byteとても小さい変数
+    byte glasses;
+    glasses = 2;
+    // short 小さい整数
+    short age;
+    age = 18;
+    // 通常の整数
+    int salary;
+    salary = 152000;
+    // long大きい整数
+    long worldPeople;
+    worldPeople = 690000000L;
+
+    System.out.println(age);
+    System.out.println(glasses);
+    System.out.println(age);
+    System.out.println(salary);
+    System.out.println(worldPeople);
   }
 }
