2540b961977cf1fd3d7212c65f0e1c9fb9b7cbd2
house-lovers7 <taisuke.gt@gmail.com>
2021-06-19 10:35:28 +0900

    コメントの2形式 /* /* と//の練習

--- Main.java
+++ Main.java
@@ -1,8 +1,13 @@
+/*
+開発者：Houser 作成日：2020年6月19日
+*/
+
 class Main {
+  // ここからメインメソッド
   public static void main(String[] args) {
     String msg = "";
     msg += "Hello ";
-    msg += "World!";
+    msg += "JAVA!";
     System.out.println(msg);
   }
 }
