e511f07f9dbe1322a4b07684aa544d16398255a7
house-lovers7 <taisuke.gt@gmail.com>
2021-06-19 11:40:09 +0900

    文字列,true&false,少数の型

--- Main.java
+++ Main.java
@@ -22,11 +22,31 @@ class Main {
     // long大きい整数
     long worldPeople;
     worldPeople = 690000000L;
+    float weight;
+    weight = 67.5F;
+    // 少数を扱ケースは、特殊な事情がない限りdoubleを使う。
+    double height;
+    height = 171.2;
+    // tureかfalseで判断できる型はboolean
+    boolean isError;
+    isError = true;
+    // charは1文字
+    char zodiac;
+    zodiac = '辰';
+    // Stringは文字の集まり
+    String name;
+    name = "すがわら";
 
     System.out.println(age);
     System.out.println(glasses);
     System.out.println(age);
     System.out.println(salary);
     System.out.println(worldPeople);
+    System.out.println(weight);
+    System.out.println(height);
+    System.out.println(isError);
+    System.out.println(zodiac);
+    System.out.println(name);
+
   }
 }
