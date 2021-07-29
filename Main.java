7b096e39d72b83b20596a725d3f06f7f22a488f8
house-lovers7 <taisuke.gt@gmail.com>
2021-07-29 11:33:47 +0900

    戻値が配列の引数をわたすコード

--- Main.java
+++ Main.java
@@ -7,31 +7,24 @@ import java.text.BreakIterator;
 
 class Main {
   // ここからメインメソッド
-    public static void main (String[] args){
-    System.out.println("ようこそ、占いの館へ");
-    System.out.println("あなたの名前を入力してください");
-    String yourname = new java.util.Scanner(System.in).nextLine();
-    System.out.println("あなたの年齢を入力してください");
-    String yourageString = new java.util.Scanner(System.in).nextLine();
-    int myage = Integer.parseInt(yourageString);
-    int fortune = new java.util.Random().nextInt(4);
-    System.out.println("占いの結果がでました!!");
-    System.out.println( myage + "歳の" + yourname + "さん、" + "あなたのいまの運気は" + fortune + "です!!"  );
-    switch (fortune) {
-      case 4:
-        System.out.println("大吉");
-        break;
-      case 3:
-        System.out.println("中吉");
-        break;
-      case 2:
-        System.out.println("吉");
-        break;
-      case 1:
-        System.out.println("凶");
-        break;
-      default:
-        System.out.println("吉");
+  public static int[] makeArray(int size) {
+    int[] newArray = new int[size];
+    /*
+    上と同じ
+    int[] newArray;
+    int型の要素を代入できる配列変数scoresを用意する
+    newArray = new int[size];
+    int型の要素をsizeの数だけ作成する
+     */
+    for (int i = 0; i < newArray.length; i++) {
+      newArray[i] = i;
+    }
+    return newArray;
+  }
+  public static void main(String[] args) {
+    int[] array = makeArray(3);
+    for(int i :array) {
+      System.out.println(i);
     }
   }
 }
